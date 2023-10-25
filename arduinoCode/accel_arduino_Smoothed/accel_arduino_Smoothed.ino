f364ddd2d42eedb6eea4243511094a5eb769521d
Peter Rogers <p.rogers@gold.ac.uk>
2023-10-25 22:29:10 +0100

    added arduino smoothed accel

--- arduinoCode/accel_arduino_Smoothed/accel_arduino_Smoothed.ino
+++ arduinoCode/accel_arduino_Smoothed/accel_arduino_Smoothed.ino
@@ -1,11 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
-
+#include <Smoothed.h> 
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
-
+Smoothed <float> accelX; 
 
 void setup() {
   Serial.begin(9600);
@@ -21,7 +21,7 @@ void setup() {
   //accel.setRange(ADXL345_RANGE_8_G);
   //accel.setRange(ADXL345_RANGE_4_G);
   accel.setRange(ADXL345_RANGE_2_G);
-  
+  accelX.begin(SMOOTHED_AVERAGE, 10);
   
   
 }
@@ -30,6 +30,8 @@ void loop() {
   /* Get a new sensor event */ 
   sensors_event_t event; 
   accel.getEvent(&event);
-  Serial.println(event.acceleration.x);
+  
+  accelX.add(event.acceleration.x);
+  Serial.println(accelX.get());
   delay(5);
 }
